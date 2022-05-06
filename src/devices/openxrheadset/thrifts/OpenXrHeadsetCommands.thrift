36a330e30d80f255fe6acb9b77b0e3c9828a11f8
Stefano <stefano.dafarra@gmail.com>
2022-05-06 18:29:42 +0200

    Added the possibility to reset the seated position from RPC.

--- src/devices/openxrheadset/thrifts/OpenXrHeadsetCommands.thrift
+++ src/devices/openxrheadset/thrifts/OpenXrHeadsetCommands.thrift
@@ -118,4 +118,10 @@ service OpenXrHeadsetCommands
     * @return True if successfull, false if the index is out of bounds
     */
     bool setLabelEnabled(1:i32 labelIndex, 2:bool enabled);
+
+    /**
+     * Align the root frame to the current headset position and angle around gravity
+     * @return True if successfull. False otherwise, e.g. if the current headset pose is not valid
+     */
+     bool alignRootFrameToHeadset(1:double distance);
 }
